81c883ce352a17de43da8838cff8979b2b3ec817
sanopera <91997129+sanopera@users.noreply.github.com>
2021-12-19 18:35:37 -0300

    desafio3

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+const NavBar = () => {
+
+    return (
+
+
+        <div className='navBar'>
+
+            <img src='./logoMadriguera.png' alt="logo"/>
+            <h2>La Madriguera</h2>
+
+            
+
+            <nav>
+                <ul>
+
+                    <li>
+                        <a href="/#">Home</a>
+                    </li>
+                    <li>
+                        <a href="/#">Universos</a>
+                    </li>
+                    <li>
+                        <a href="/#">MadriBox</a>
+                    </li>
+
+                </ul>
+
+            </nav>
+
+        </div>
+    )
+
+}
+
+
+export default NavBar
